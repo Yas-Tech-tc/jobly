f74324b3253f3871dd14a25065cc0115ef9a54d3
Yas-Tech-tc <yas073553@gmail.com>
2026-06-26 00:23:33 +0100

    Add JobList component

---
+++ v2-react/src/components/JobList.jsx
@@ -0,0 +1,21 @@
+import JobCard from './JobCard'
+
+function JobList({ jobs }) {
+    if (jobs.length === 0) {
+        return (
+            <p className="no-results">
+                No jobs found matching your search.
+            </p>
+        )
+    }
+
+    return (
+        <div className="jobs-grid">
+            {jobs.map((job) => (
+                <JobCard key={job.id} job={job} />
+            ))}
+        </div>
+    )
+}
+
+export default JobList
