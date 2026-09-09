136d588c201ae3e52ee7955c4a6d2b9fe9fe8c7b
yasmine <yas073553@gmail.com@gmail.com>
2026-09-09 19:50:23 +0100

    Fix CORS for production

--- v3-server/server.js
+++ v3-server/server.js
@@ -11,7 +11,13 @@ const authMiddleware = require('./middleware/auth')
 const app = express()
 const PORT = process.env.PORT || 3001
 
-app.use(cors())
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        'https://jobly-ruby.vercel.app'
+    ],
+    credentials: true
+}))
 app.use(express.json())
 
 mongoose.connect(process.env.MONGODB_URI)
